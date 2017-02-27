ddb84724e4346493bba9e1880eeb7a5157ece8e7
truskulowylak <akarolak@mion.elka.pw.edu.pl>
2017-02-27 21:06:57 +0100

    Issue: poprawiony plik cmd

--- start.cmd
+++ start.cmd
@@ -1,9 +1,9 @@
-START Simulet.jar Lampka 
-START Simulet.jar Samochod 
-START Simulet.jar Radio 
-START Simulet.jar Wiatraczek 
-START Simulet.jar Trigger_1 
-START Simulet.jar Trigger_2 
+START target/Simulet-1.0.0-SNAPSHOT.jar Lampka 
+START target/Simulet-1.0.0-SNAPSHOT.jar Samochod 
+START target/Simulet-1.0.0-SNAPSHOT.jar Radio 
+START target/Simulet-1.0.0-SNAPSHOT.jar Wiatraczek 
+START target/Simulet-1.0.0-SNAPSHOT.jar Trigger_1 
+START target/Simulet-1.0.0-SNAPSHOT.jar Trigger_2 
 
 
 
