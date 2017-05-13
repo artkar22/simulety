69bba21732a2bec3818befe1bcd4bb82b1a5ac79
truskulowylak <akarolak@mion.elka.pw.edu.pl>
2017-05-13 16:20:50 +0200

    Issue: poprawiony parametr opozniajacy uruchamianie simuletu

--- start.cmd
+++ start.cmd
@@ -1,13 +1,13 @@
 START target/Simulet-1.0.0-SNAPSHOT.jar Lampka 
-timeout 1
+timeout 2
 START target/Simulet-1.0.0-SNAPSHOT.jar Samochod 
-timeout 1
+timeout 2
 START target/Simulet-1.0.0-SNAPSHOT.jar Radio 
-timeout 1
+timeout 2
 START target/Simulet-1.0.0-SNAPSHOT.jar Wiatraczek 
-timeout 1
+timeout 2
 START target/Simulet-1.0.0-SNAPSHOT.jar Trigger_1 
-timeout 1
+timeout 2
 START target/Simulet-1.0.0-SNAPSHOT.jar Trigger_2 
 
 
