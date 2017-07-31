b530b792a8012592a4db5b29f183882c1cba6d8e
truskulowylak <akarolak@mion.elka.pw.edu.pl>
2017-07-31 11:08:21 +0200

    Issue: trabka,radio, przycisk mono

---
+++ start - monostable, radio, trąbka.cmd
@@ -0,0 +1,11 @@
+START target/Simulet-1.0.0-SNAPSHOT.jar Trabka 
+timeout 2
+START target/Simulet-1.0.0-SNAPSHOT.jar Radio 
+timeout 2
+START target/Simulet-1.0.0-SNAPSHOT.jar MONOSTABLE_TRIGGER 
+timeout 2
+START target/Simulet-1.0.0-SNAPSHOT.jar Trigger_2 
+
+
+
+
