86dd64eb61a056b59dcf302745fe2be91b40823f
truskulowylak <akarolak@mion.elka.pw.edu.pl>
2017-08-30 22:46:57 +0200

    Issue: simulet counter

---
+++ src/main/java/app/PossibleStatesConsts.java
@@ -0,0 +1,10 @@
+package app;
+
+/**
+ * Created by Artur Karolak on 2017-08-30.
+ */
+public class PossibleStatesConsts {
+    private PossibleStatesConsts(){}
+
+    public static final String START_TIMER = "START_TIMER";
+}
